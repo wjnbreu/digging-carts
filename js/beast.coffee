0cf849fcf4311b61d7c5da3777fe55acc4291868
James Pants <jamespants@gmail.com>
2014-08-06 15:04:09 +0200

    add videos

--- js/beast.coffee
+++ js/beast.coffee
@@ -1,8 +1,61 @@
 $ ->
+
+	dataObject = {}
+	player = {}
+
+	getData = ->
+		$.ajax 'data/data.json',
+			type: 'GET'
+			error: (jqXHR, textStatus, errorThrown) ->
+				console.log "AJAX Error: #{textStatus}"
+			success: (data, textStatus, jqXHR) ->
+				dataObject = data
+				init()
+	
 	init = ->
+		setupYouTube()
 		setupBinds()
+		
+
+	# addData = (item, data) ->
+	# 	#get video based on order
+	# 	video = data.videos[item]
+	# 	currentVideo = {
+	# 		body: video.body
+	# 		id: video.id
+	# 		order: video.order
+	# 		title: video.title
+	# 	}
+	# 	return currentVideo
+
+		
+	setupYouTube = ->
+		tag = document.createElement('script')
+		tag.src = "https://www.youtube.com/iframe_api"
+		firstScriptTag = document.getElementsByTagName('script')[0]
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+
+
+	window.onYouTubeIframeAPIReady = ->
+		player = new YT.Player 'player',
+			height: '390'
+			width: '640'
+			videoId: 'FuLTIi7CyOk'
+			events: {
+				"onReady": onPlayerReady
+			}
+			playerVars: {
+				modestbranding: true
+				controls: 0
+				showinfo: 0
+			}
+
+	onPlayerReady = (event) ->
+		resizeVid()
+		
 
 	setupBinds = ->
+		console.log dataObject
 		$('nav').bind 'mouseenter', ->
 			$(@).transition
 				left: 0
@@ -11,9 +64,35 @@ $ ->
 			$(@).transition
 				left: '-100px'
 			, 200
+		$('a.episode').bind 'click', ->
+			order = $(@).data 'order'
+			changeVideo(order)
+
+	changeVideo = (order) ->
+		video = dataObject.videos
+		video = video[order]
+		player.cueVideoById(video.id)
+
+		$('.videos h2').empty().text video.title
+		$('.videos p.body').empty().text video.body
+
+	resizeVid = ->
+		winWidth = $(window).width()
+		vidWidth = winWidth / 1.5
+		ogWidth = $('#player').attr('width')
+		ogHeight = $('#player').attr('height')
+		ratio = ogWidth / ogHeight
+
+		$('#player').attr('width', vidWidth)
+		$('#player').attr('height', vidWidth / ratio)
+
+		diff = winWidth - vidWidth
+		margin = diff / 2
+		$('#player').css
+			marginLeft: margin
 
 
-	init()
+	getData()
 
 
 	
--- js/beast.js
+++ js/beast.js
@@ -1,1 +1,90 @@
-(function(){$(function(){var n,t;return n=function(){return t()},t=function(){return $("nav").bind("mouseenter",function(){return $(this).transition({left:0},200)}),$("nav").bind("mouseleave",function(){return $(this).transition({left:"-100px"},200)})},n()})}).call(this);
+(function() {
+    $(function() {
+        var t, e, n, r, i, o, a, u, c;
+        e = {};
+        o = {};
+        n = function() {
+            return $.ajax("data/data.json", {
+                type: "GET",
+                error: function(t, e, n) {
+                    return console.log("AJAX Error: " + e);
+                },
+                success: function(t, n, i) {
+                    e = t;
+                    return r();
+                }
+            });
+        };
+        r = function() {
+            c();
+            return u();
+        };
+        c = function() {
+            var t, e;
+            e = document.createElement("script");
+            e.src = "https://www.youtube.com/iframe_api";
+            t = document.getElementsByTagName("script")[0];
+            return t.parentNode.insertBefore(e, t);
+        };
+        window.onYouTubeIframeAPIReady = function() {
+            return o = new YT.Player("player", {
+                height: "390",
+                width: "640",
+                videoId: "FuLTIi7CyOk",
+                events: {
+                    onReady: i
+                },
+                playerVars: {
+                    modestbranding: true,
+                    controls: 0,
+                    showinfo: 0
+                }
+            });
+        };
+        i = function(t) {
+            return a();
+        };
+        u = function() {
+            console.log(e);
+            $("nav").bind("mouseenter", function() {
+                return $(this).transition({
+                    left: 0
+                }, 200);
+            });
+            $("nav").bind("mouseleave", function() {
+                return $(this).transition({
+                    left: "-100px"
+                }, 200);
+            });
+            return $("a.episode").bind("click", function() {
+                var e;
+                e = $(this).data("order");
+                return t(e);
+            });
+        };
+        t = function(t) {
+            var n;
+            n = e.videos;
+            n = n[t];
+            o.cueVideoById(n.id);
+            $(".videos h2").empty().text(n.title);
+            return $(".videos p.body").empty().text(n.body);
+        };
+        a = function() {
+            var t, e, n, r, i, o, a;
+            a = $(window).width();
+            o = a / 1.5;
+            r = $("#player").attr("width");
+            n = $("#player").attr("height");
+            i = r / n;
+            $("#player").attr("width", o);
+            $("#player").attr("height", o / i);
+            t = a - o;
+            e = t / 2;
+            return $("#player").css({
+                marginLeft: e
+            });
+        };
+        return n();
+    });
+}).call(this);
