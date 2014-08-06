c94f55fa14ec9c45c99268209bd48af9ce4d5982
James Pants <jamespants@gmail.com>
2014-08-06 19:37:33 +0200

    first commit

---
+++ js/beast.coffee
@@ -0,0 +1,200 @@
+$ ->
+
+	dataObject = {}
+	player = {}
+	anchorElements = {}
+
+	hoverSound = new Howl {
+		urls: ['sound/CLICK.mp3', 'sound/CLICK.ogg']
+		volume: 0.5
+	}
+
+	clickSound = new Howl {
+		urls: ['sound/EXIT.mp3', 'sound/EXIT.ogg']
+		volume: 0.7
+	}
+
+	# $('.hero').waypoint ->
+	# 	$('.content').stop().animate
+	# 		backgroundColor: 'black'
+	# 	,500
+
+	# $('#videos').waypoint ->
+	# 	$('.content').stop().animate
+	# 		backgroundColor: 'black'
+	# 	,500
+
+	# # $('#composers').waypoint ->
+	# # 	$('.content').stop().animate
+	# # 		backgroundColor: 'blue'
+	# # 	,500
+
+	# $('#radio').waypoint ->
+	# 	$('.content').stop().animate
+	# 		backgroundColor: 'black'
+	# 	, 500
+	# 	$('.content').css
+	# 		background: 'url(img/falling-sky.gif) repeat top left'
+
+	getData = ->
+		$.ajax 'data/data.json',
+			type: 'GET'
+			error: (jqXHR, textStatus, errorThrown) ->
+				console.log "AJAX Error: #{textStatus}"
+			success: (data, textStatus, jqXHR) ->
+				dataObject = data
+				init()
+	
+	init = ->
+		setupYouTube()
+		setupBinds()
+		addComposers()
+		
+		
+	setupYouTube = ->
+		tag = document.createElement('script')
+		tag.src = "https://www.youtube.com/iframe_api"
+		firstScriptTag = document.getElementsByTagName('script')[0]
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+
+
+	window.onYouTubeIframeAPIReady = ->
+		player = new YT.Player 'player',
+			height: '390'
+			width: '640'
+			videoId: 'FuLTIi7CyOk'
+			events: {
+				"onReady": onPlayerReady
+			}
+			playerVars: {
+				modestbranding: 1
+				controls: 1
+				showinfo: 0
+				hd: 1
+
+			}
+
+	onPlayerReady = (event) ->
+		resizeVid()
+		
+
+	setupBinds = ->
+		$('nav').bind 'mouseenter', ->
+			$(@).transition
+				left: 0
+			, 200
+
+		$('a').bind 'click', ->
+			clickSound.play()
+		
+		$('a').bind 'mouseenter', ->
+			hoverSound.play()
+
+		$('nav').bind 'mouseleave', ->
+			$(@).transition
+				left: '-100px'
+			, 200
+		
+		$('a.episode').bind 'click', ->
+			order = $(@).data 'order'
+			changeVideo(order)
+
+		$('.composer-title').bind 'click', ->
+			$(@).parent().find('.composer-nav').slideToggle()
+		
+		$('a.composer').bind 'click',(event) ->
+			event.preventDefault()
+			if $(@).find('li').hasClass "active"
+				$('.composer-data').slideUp()
+				$(@).find('li').removeClass "active"
+			else
+				$('.composer-data').each ->
+					$(@).slideUp()
+					$('a.composer').find('li').removeClass "active"
+				$(@).find('.composer-data').slideToggle()
+				$(@).find('li').toggleClass 'active'
+
+				location = $("#composers").offset().top + 100
+				$('body,html').animate
+					scrollTop: location
+				, 500
+
+		$('a.scroll').bind 'click', (event) ->
+			link = $(@)
+			smoothScroll(event, link)
+
+		# $(document).bind 'scroll', (event) ->
+		# 	console.log $('#rapper').offset().top
+			
+			
+	sendScore = ->
+		scoreFactor = Math.floor(Math.random() * -1000)
+		points = ($('.hero').position().top * scoreFactor)
+		if points >= $('.score').find('h2 span').text()
+
+			$('.score').find('h2 span').empty().text(points)
+
+
+	changeVideo = (order) ->
+		video = dataObject.videos
+		video = video[order]
+		player.cueVideoById(video.id)
+
+		$('.videos h2').empty().text video.title
+		$('.videos p.body').empty().text video.body
+		$('.videos p.body').slideDown()
+
+	addComposers = (order,item) ->
+		composers = $('.composer-nav ul li')
+		composers.each (index) ->
+			t = $(@)
+			#account for zero indexing
+			person = dataObject.composers[index + 1]
+			name = person.name
+			t.text(name)
+			img = "img/#{person.image}"
+			composerData = "<div class='composer-data'><img src='#{img}'/><p>#{person.bio}</p></div>"
+			t.append composerData
+
+
+
+	resizeVid = ->
+		winWidth = $(window).width()
+		vidWidth = winWidth / 1.5
+		ogWidth = $('#player').attr('width')
+		ogHeight = $('#player').attr('height')
+		ratio = ogWidth / ogHeight
+
+		$('#player').attr('width', vidWidth)
+		$('#player').attr('height', vidWidth / ratio)
+
+		diff = winWidth - vidWidth
+		margin = diff / 2
+		$('#player').css
+			marginLeft: margin
+
+	smoothScroll = (event, link) ->
+		event.preventDefault()
+		scrollTo = link.attr 'href'
+
+		location = $("#{scrollTo}").position().top
+
+		if link.hasClass "active"
+			return
+		else
+			$('nav ul a').each ->
+				$(@).removeClass "active"
+
+			link.addClass "active"
+
+			$('body,html').animate
+				scrollTop: location
+			, 300
+
+
+	getData()
+
+
+	
+		
+	
---
+++ js/beast.js
@@ -0,0 +1,171 @@
+(function() {
+    $(function() {
+        var e, t, n, r, o, i, a, s, c, u, l, d, p, f, m;
+        o = {};
+        u = {};
+        t = {};
+        a = new Howl({
+            urls: [ "sound/CLICK.mp3", "sound/CLICK.ogg" ],
+            volume: .5
+        });
+        r = new Howl({
+            urls: [ "sound/EXIT.mp3", "sound/EXIT.ogg" ],
+            volume: .7
+        });
+        i = function() {
+            return $.ajax("data/data.json", {
+                type: "GET",
+                error: function(e, t, n) {
+                    return console.log("AJAX Error: " + t);
+                },
+                success: function(e, t, n) {
+                    o = e;
+                    return s();
+                }
+            });
+        };
+        s = function() {
+            f();
+            p();
+            return e();
+        };
+        f = function() {
+            var e, t;
+            t = document.createElement("script");
+            t.src = "https://www.youtube.com/iframe_api";
+            e = document.getElementsByTagName("script")[0];
+            return e.parentNode.insertBefore(t, e);
+        };
+        window.onYouTubeIframeAPIReady = function() {
+            return u = new YT.Player("player", {
+                height: "390",
+                width: "640",
+                videoId: "FuLTIi7CyOk",
+                events: {
+                    onReady: c
+                },
+                playerVars: {
+                    modestbranding: 1,
+                    controls: 1,
+                    showinfo: 0,
+                    hd: 1
+                }
+            });
+        };
+        c = function(e) {
+            return l();
+        };
+        p = function() {
+            $("nav").bind("mouseenter", function() {
+                return $(this).transition({
+                    left: 0
+                }, 200);
+            });
+            $("a").bind("click", function() {
+                return r.play();
+            });
+            $("a").bind("mouseenter", function() {
+                return a.play();
+            });
+            $("nav").bind("mouseleave", function() {
+                return $(this).transition({
+                    left: "-100px"
+                }, 200);
+            });
+            $("a.episode").bind("click", function() {
+                var e;
+                e = $(this).data("order");
+                return n(e);
+            });
+            $(".composer-title").bind("click", function() {
+                return $(this).parent().find(".composer-nav").slideToggle();
+            });
+            $("a.composer").bind("click", function(e) {
+                var t;
+                e.preventDefault();
+                if ($(this).find("li").hasClass("active")) {
+                    $(".composer-data").slideUp();
+                    return $(this).find("li").removeClass("active");
+                } else {
+                    $(".composer-data").each(function() {
+                        $(this).slideUp();
+                        return $("a.composer").find("li").removeClass("active");
+                    });
+                    $(this).find(".composer-data").slideToggle();
+                    $(this).find("li").toggleClass("active");
+                    t = $("#composers").offset().top + 100;
+                    return $("body,html").animate({
+                        scrollTop: t
+                    }, 500);
+                }
+            });
+            return $("a.scroll").bind("click", function(e) {
+                var t;
+                t = $(this);
+                return m(e, t);
+            });
+        };
+        d = function() {
+            var e, t;
+            t = Math.floor(Math.random() * -1e3);
+            e = $(".hero").position().top * t;
+            if (e >= $(".score").find("h2 span").text()) {
+                return $(".score").find("h2 span").empty().text(e);
+            }
+        };
+        n = function(e) {
+            var t;
+            t = o.videos;
+            t = t[e];
+            u.cueVideoById(t.id);
+            $(".videos h2").empty().text(t.title);
+            $(".videos p.body").empty().text(t.body);
+            return $(".videos p.body").slideDown();
+        };
+        e = function(e, t) {
+            var n;
+            n = $(".composer-nav ul li");
+            return n.each(function(e) {
+                var t, n, r, i, a;
+                a = $(this);
+                i = o.composers[e + 1];
+                r = i.name;
+                a.text(r);
+                n = "img/" + i.image;
+                t = "<div class='composer-data'><img src='" + n + "'/><p>" + i.bio + "</p></div>";
+                return a.append(t);
+            });
+        };
+        l = function() {
+            var e, t, n, r, o, i, a;
+            a = $(window).width();
+            i = a / 1.5;
+            r = $("#player").attr("width");
+            n = $("#player").attr("height");
+            o = r / n;
+            $("#player").attr("width", i);
+            $("#player").attr("height", i / o);
+            e = a - i;
+            t = e / 2;
+            return $("#player").css({
+                marginLeft: t
+            });
+        };
+        m = function(e, t) {
+            var n, r;
+            e.preventDefault();
+            r = t.attr("href");
+            n = $("" + r).position().top;
+            if (t.hasClass("active")) {} else {
+                $("nav ul a").each(function() {
+                    return $(this).removeClass("active");
+                });
+                t.addClass("active");
+                return $("body,html").animate({
+                    scrollTop: n
+                }, 300);
+            }
+        };
+        return i();
+    });
+}).call(this);
